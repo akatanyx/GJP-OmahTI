3feba14d471ba8de7ce95a992cf23292b12b06e4
Rionaldi <72600759+rionaldimuh@users.noreply.github.com>
2022-11-19 15:57:17 +0700

    Melee Attack Phase 1

--- Assets/Scripts/_Player/PlayerAttack/CharacterAttack.cs
+++ Assets/Scripts/_Player/PlayerAttack/CharacterAttack.cs
@@ -5,24 +5,38 @@ using UnityEngine;
 public class CharacterAttack : Character
 {
     private bool isAttacking, canAttack = true;
+    public bool isEnemyInProximity;
 
+    [SerializeField]
+    private float AttackDowntime = 0.5f;
 
     #region Ranged Attack
 
     public GameObject bulletSpawner;
     private int rangedAttackTime;
     [SerializeField]
-    private int maxRangedAttackTime;
-    [SerializeField]
-    private float rangedAttackCooldown;
+    private int maxRangedAttackTime = 4;
 
     public ObjectPooling pooler { get; set; }
 
 
     #endregion
+
+    #region Melee Attack
+
+    private int meleeAttackTime;
+    [SerializeField]
+    private int maxMeleeAttackTime = 3;
+    [SerializeField]
+    private GameObject meleeAttackObject;
+    private Collider2D meleeCol;
+
+    #endregion
     private void Start()
     {
         pooler = GetComponent<ObjectPooling>();
+        //meleeCol = meleeAttackObject.GetComponent<Collider2D>();
+        //meleeCol.enabled = false;
     }
 
     // Update is called once per frame
@@ -37,35 +51,45 @@ public class CharacterAttack : Character
     {
         if (inputManager.AttackPressed())
         {
-            anim.SetBool("AttackRanged", true);
+            if (!isEnemyInProximity)
+            {
+                anim.SetBool("AttackRanged", true);
+            }
+            else
+            {
+                anim.SetBool("AttackMelee", true);
+            }
             isAttacking = true;
         }
         else if (inputManager.AttackKeyUp())
-            StartCoroutine(RangedAttackCooldown());
-        //else
-        //{
-        //    StartCoroutine(RangedAttackCooldown());
-        //}
+        {
+            StartCoroutine(AttackCooldown());
+        }
+    }
+    IEnumerator AttackCooldown()
+    {
+        canAttack = false;
+        isAttacking = false;
+        rangedAttackTime = 0;
+        meleeAttackTime = 0;
+        anim.SetBool("AttackRanged", false);
+        anim.SetBool("AttackMelee", false);
+        yield return new WaitForSeconds(AttackDowntime);
+        canAttack = true;
     }
 
+
+    #region Ranged Attack Function
+
+    //Diassign ke animation
     void SpawnBullet()
     {
         rangedAttackTime++;
         spawnProjectile(bulletSpawner.transform.position);
         if(rangedAttackTime == maxRangedAttackTime)
         {
-            StartCoroutine(RangedAttackCooldown());
+            StartCoroutine(AttackCooldown());
         }
-    }
-
-    IEnumerator RangedAttackCooldown()
-    {
-        canAttack = false;
-        isAttacking = false;
-        rangedAttackTime = 0;
-        anim.SetBool("AttackRanged", false);
-        yield return new WaitForSeconds(rangedAttackCooldown);
-        canAttack = true;
     }
 
     void spawnProjectile(Vector2 spawnPosition)
@@ -78,6 +102,32 @@ public class CharacterAttack : Character
         Vector2 newDirection = flip.isFlipped ? -transform.right : transform.right;
 
         projectile.SetDirection(newDirection, transform.rotation);
-        
     }
+
+    #endregion
+
+    #region Melee Attack Function
+
+    //Diassign ke animation
+    void MeleeAttack()
+    {
+        meleeAttackTime++;
+        if (meleeAttackTime == maxMeleeAttackTime)
+        {
+            StartCoroutine(AttackCooldown());
+        }
+    }
+
+    //Diassign ke animation
+    void ShowMeleeAttackCollider()
+    {
+        //meleeCol.enabled = true;
+        meleeAttackObject.SetActive(true);
+    }
+    void UnShowMeleeAttackCollider()
+    {
+        //meleeCol.enabled = false;
+        meleeAttackObject.SetActive(false);
+    }
+    #endregion
 }
