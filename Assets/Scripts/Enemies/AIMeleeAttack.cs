b6557d1086181a8b1123e66075b3cfb9789198e6
Rionaldi <72600759+rionaldimuh@users.noreply.github.com>
2022-11-19 23:40:08 +0700

    Add Knockback for Anti Melee

--- Assets/Scripts/Enemies/AIMeleeAttack.cs
+++ Assets/Scripts/Enemies/AIMeleeAttack.cs
@@ -9,7 +9,7 @@ public class AIMeleeAttack : AIManagers
     protected bool hitPlayerWhenClose;
     //How much damage needs to be applied to the Player when the Player is hit by the attack
     [SerializeField]
-    protected int damageAmount;
+    protected int damageAmount; 
 
     //A collider that gets adjusted through an animation to determine if the Player is inside that collider
     [SerializeField] protected Collider2D swipeCollider;
@@ -21,6 +21,13 @@ public class AIMeleeAttack : AIManagers
     //protected PlayerHealth playerHealth;
     //A quick bool that turns true if the melee attack struck the Player
     protected bool hit;
+
+    [SerializeField]
+    protected bool canKnockback;
+    [SerializeField]
+    protected int knockbackForce;
+    [SerializeField]
+    protected float knockbackTime;
     void Start()
     {
         Initialization();
@@ -46,11 +53,24 @@ public class AIMeleeAttack : AIManagers
         if (collision.gameObject.tag == "Player" && !hit)
         {
             hit = true;
-            Debug.Log("Hit by Enemy");
             collision.GetComponent<HealthManager>().DealDamage(damageAmount);
+            if (canKnockback)
+            {
+                Rigidbody2D enemyRB = collision.GetComponent<Rigidbody2D>();
+                CharacterMovement enemyMovement = collision.GetComponent<CharacterMovement>();
+
+                //Matikan Movement Karakter biar bisa knockback karena bermasalah dengan fixedupdate
+                StartCoroutine(Knockback(enemyMovement));
+
+                //Kalkulasi
+                Vector2 direction = (collision.transform.position - transform.position);
+                direction = direction.normalized * knockbackForce;
+                enemyRB.AddForce(direction, ForceMode2D.Impulse);
+            }
         }
     }
 
+  
     //This method manages if and when the Enemy should attack the Player; most of this logic is managed by whether or not the Enemy should attack only when close, and how much time is left in the timeTillDoAction variable
     protected virtual void HitPlayer()
     {
@@ -74,6 +94,12 @@ public class AIMeleeAttack : AIManagers
         //Invoke("CancelSwipe", anim.GetCurrentAnimatorStateInfo(0).length);
     }
 
+    IEnumerator Knockback(CharacterMovement movement)
+    {
+        movement.enabled = false;
+        yield return new WaitForSeconds(knockbackTime);
+        movement.enabled = true;
+    }
 
     //Manages the animation and disables the swipe game object from the scene until the Enemy melee attacks again
     protected virtual void CancelSwipe()
